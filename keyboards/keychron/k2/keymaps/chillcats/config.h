cf29779b9ad2192370eb16adb81f4dee5ca2a654
chill-cats <public.hoangnhanlenguyen@gmail.com>
2022-03-10 23:14:47 +0700

    Add tap dance for some features

---
+++ keyboards/keychron/k2/keymaps/chillcats/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define COMBO_COUNT 3
+#define TAP_DANCE_ENABLE 1
+#define TAPPING_TERM 200
--- keyboards/keychron/k2/keymaps/chillcats/keymap.c
+++ keyboards/keychron/k2/keymaps/chillcats/keymap.c
@@ -20,10 +20,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdint.h>
 #include "../../rgb/ansi/ansi.h"
 #include "../../rgb/config.h"
+#include "./config.h"
 
 #include "action_layer.h"
 #include "action.h"
 #include "action_util.h"
+#include "rgb_matrix.h"
+#include "process_combo.h"
+#include "process_tap_dance.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -34,7 +38,9 @@ enum layer_names {
     WIN_FN   = 1,
     MAC_BASE = 2,
     MAC_FN   = 3,
+    // MAC_FN_2 = 4,
 };
+
 #define KC_TASK LGUI(KC_TAB)        // Task viewer
 #define KC_FLXP LGUI(KC_E)          // Windows file explorer
 
@@ -43,7 +49,40 @@ enum layer_names {
 #define KC_MSCR LSFT(LGUI(KC_3))    // Mac screenshot
 #define KC_MSNP LSFT(LGUI(KC_4))    // Mac snip tool
 
+// =================== COMBO ===================================
+static const uint16_t PROGMEM semiFJCombo[] = { KC_F, KC_J, COMBO_END };
+static const uint16_t PROGMEM tabDKCombo[] = { KC_D, KC_K, COMBO_END };
+static const uint16_t PROGMEM escJKCombo[] = { KC_J, KC_K, COMBO_END };
 
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(semiFJCombo, KC_SEMICOLON),
+    COMBO(tabDKCombo, KC_TAB),
+    COMBO(escJKCombo, KC_ESC),
+};
+
+// ================== COMBO END =================================
+// ================== TAP DANCE =================================
+typedef enum {
+  TD_NONE,
+  TD_UNKNOWN,
+  TD_SINGLE_TAP,
+  TD_SINGLE_HOLD,
+} td_state_t;
+
+typedef struct {
+  td_state_t state;
+} td_tap_t;
+
+enum {
+    TD_CAPS_ESC,
+};
+
+td_state_t current_dance(qk_tap_dance_state_t *state);
+
+void caps_finished(qk_tap_dance_state_t *state, void *user_data);
+void caps_reset(qk_tap_dance_state_t *state, void *user_data);
+
+// ================== TAP DANCE END =============================
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /*
@@ -112,22 +151,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *****************************************************************************************************************
   */
   [MAC_BASE] = LAYOUT_75_ansi(
-  /*  0           1           2           3           4           5           6           7           8           9           10          11          12          13          14          15       */
-      KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_MSCR,    KC_DEL,     RGB_MOD  ,
-      KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                KC_PGUP  ,
-      KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                KC_PGDN  ,
-      KC_ESC,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                KC_ENT,                 KC_HOME  ,
-      KC_LSFT,                KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RSFT,    KC_UP,      KC_END   ,
-      KC_LCTL,    KC_LALT,    KC_LGUI,                                        KC_SPC,                                         KC_RGUI,    MO(MAC_FN), KC_RCTL,    KC_LEFT,    KC_DOWN,    KC_RGHT
+  /*  0           1           2           3           4           5           6           7           8           9           10          11          12               13          14          15       */
+      KC_ESC,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,          KC_MSCR,    KC_DEL,     RGB_MOD  ,
+      KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,          KC_BSPC,                KC_PGUP  ,
+      KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,         KC_BSLS,                KC_PGDN  ,
+      TD(TD_CAPS_ESC),     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                     KC_ENT,                 KC_HOME  ,
+      KC_LSFT,                KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,                     KC_RSFT,    KC_UP,      KC_END   ,
+      KC_LCTL,    KC_LALT,    KC_LGUI,                                        KC_SPC,                                         KC_RGUI,    MO(MAC_FN), KC_RCTRL,        KC_LEFT,    KC_DOWN,    KC_RGHT
   ),
 
   /*
     *****************************************************************************************************************
     *RESET * BRID * BRIU * TASK * FLXP * RVAD * RVAI * MPRV * MPLY * MNXT * MUTE * VOLD * VOLU *      *  INS * RTOG *
     *****************************************************************************************************************
-    *      *      *      *      *      *      *      *      *      *      *      *      *      *             *      *
+    *      *      *      *      *      *      *      *      *      *      *      *      *      *             * BRIU *
     *****************************************************************************************************************
-    *         *      *      *      *      *      *      *      *      *      *      *      *      *          *      *
+    *         *      *      *      *      *      *      *      *      *      *      *      *      *          * BRID *
     *****************************************************************************************************************
     *           *      *      *      *      *      *      *      *      *      *      *      *               *      *
     *****************************************************************************************************************
@@ -139,14 +178,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [MAC_FN] = LAYOUT_75_ansi(
   /*  0           1           2           3           4           5           6           7           8           9           10          11          12          13          14          15       */
       RESET,      KC_BRID,    KC_BRIU,    KC_MSSN,    KC_FIND,    RGB_VAD,    RGB_VAI,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_MSNP,    KC_INS,     RGB_TOG  ,
-      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______  ,
-      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______  ,
+      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                KC_BRIU  ,
+      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                KC_BRID  ,
       _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______,                _______  ,
       _______,                _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______,    RGB_SAI,    _______  ,
       _______,    _______,    _______,                                        _______,                                        _______,    _______,    _______,    RGB_HUD,    RGB_SAD,    RGB_HUI
-  )
+  ),
 };
-
 bool dip_switch_update_user(uint8_t index, bool active) {
   switch(index) {
     case 0: // OS switch
@@ -170,39 +208,120 @@ bool dip_switch_update_user(uint8_t index, bool active) {
   return true;
 }
 
-// void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  // debug_enable = true;
-  // debug_matrix = true;
-  // debug_keyboard = true;
-  // debug_mouse = true;
-// }
+void setDefaultLedColorOnBoot(void) {
+    rgb_matrix_enable_noeeprom();
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_sethsv_noeeprom(16, 200, 200);
+}
+
+void keyboard_post_init_user(void) {
+//   Customise these values to desired behaviour
+//   debug_enable = true;
+//   debug_matrix = true;
+//   debug_keyboard = true;
+//   debug_mouse = true;
+    setDefaultLedColorOnBoot();
+}
+
+// bool cmdPressed = false;
+
+// bool cmdHoldActivated = false;
+
+// uint16_t cmdHoldTimer = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    uint8_t modState =  get_mods();
-    switch(keycode) {
-        case KC_BSPACE: {
-            static bool delKeyRegistered;
+    // switch(keycode) {
+    //     case KC_LGUI: {
+    //         if (record->event.pressed) {
+    //             if (!cmdPressed) {
+    //                 cmdPressed = true;
+    //             }
+    //             register_code(KC_LGUI);
+    //             return false;
+    //         } else {
+    //             if (!cmdHoldActivated) {
+    //                 cmdPressed = false;
+    //                 unregister_code(KC_LGUI);
+    //             }
+    //             return false;
+    //         }
+    //         break;
+    //     }
+    //     case KC_TAB: {
+    //         if (record->event.pressed) {
+    //             if (cmdPressed == true) {
+    //                 cmdPressed = false;
+    //                 cmdHoldActivated = true;
+    //                 register_code(KC_TAB);
+    //                 return false;
+    //             }
 
-            if (record->event.pressed) {
-                if (modState & MOD_MASK_SHIFT) {
+    //             if (cmdHoldActivated) {
+    //                 register_code(KC_TAB);
+    //                 return false;
+    //             }
+    //         }
+    //         break;
+    //     }
 
-                    // unregister SHIFT key to prevent interaction with BKSPACE
-                    del_mods(MOD_MASK_SHIFT);
+    //     default: {
+    //         if (cmdHoldActivated) {
+    //             unregister_code(KC_LGUI);
+    //         }
+    //         cmdHoldActivated = false;
 
-                    register_code(KC_DEL);
-                    delKeyRegistered = true;
-
-                    set_mods(modState);
-                    return false;
-                }
-            } else if (delKeyRegistered) {
-                    unregister_code(KC_DEL);
-                    delKeyRegistered = false;
-                    return false;
-            }
-            return true;
-        }
-    }
+    //     }
+    // }
     return true;
 }
+// ================== TAP DANCE =================================
+td_state_t current_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) {
+            return TD_SINGLE_TAP;
+        }
+        return TD_SINGLE_HOLD;
+    }
+    return TD_UNKNOWN;
+}
+
+static td_tap_t caps_tap_state = {
+    .state = TD_NONE,
+};
+
+void caps_finished(qk_tap_dance_state_t *state, void *user_data) {
+    caps_tap_state.state = current_dance(state);
+    switch (caps_tap_state.state)
+    {
+    case TD_SINGLE_TAP:
+        register_code(KC_ESC);
+        break;
+    case TD_SINGLE_HOLD:
+        register_code(KC_CAPS);
+        break;
+    default:
+        break;
+    }
+}
+
+void caps_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch(caps_tap_state.state) {
+        case TD_SINGLE_TAP:
+            unregister_code(KC_ESC);
+            break;
+        case TD_SINGLE_HOLD:
+            unregister_code(KC_CAPS);
+            break;
+        default:
+            break;
+    }
+    caps_tap_state.state = TD_NONE;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_CAPS_ESC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, caps_finished, caps_reset),
+};
+
+// ================== TAP DANCE END =============================
+
+
